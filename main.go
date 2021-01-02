155135a44f3fd10a6e74cf65e2a05180f1d4dcc6
Padrition <padrition@gmail.com>
2021-01-02 18:46:22 +0600

    added html file and abbility to send custom messages through this html

--- main.go
+++ main.go
@@ -2,30 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/smtp"
 )
 
-func main() {
-	fmt.Println("Dima huima")
-
+func sendMail(rw http.ResponseWriter, r *http.Request) {
 	from := "ebanyvrotblyatvashegocasino@gmail.com"
 	password := "A123456789b"
-
-	to := []string{
-		"padrition@gmail.com",
-	}
 
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
 
-	message := []byte("This is a message.")
-
-	auth := smtp.PlainAuth("", from, password, smtpHost)
-
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if err != nil {
-		fmt.Println(err)
-		return
+	switch r.Method {
+	case "GET":
+		http.ServeFile(rw, r, "resources/send.html")
+	case "POST":
+		receiverEmail := []string{r.FormValue("receiverEmail")}
+		message := []byte(r.FormValue("message"))
+		auth := smtp.PlainAuth("", from, password, smtpHost)
+		err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, receiverEmail, message)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Email Sent Successfully!")
 	}
-	fmt.Println("Email Sent Successfully!")
+}
+
+func setupRouts() {
+	http.HandleFunc("/send", sendMail)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	setupRouts()
 }
