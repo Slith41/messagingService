2aeed9b45aca45c9e7269ecf593f2958a560f90f
Padrition <padrition@gmail.com>
2021-01-04 20:22:22 +0600

    moved send function from sendingMessage.go to routes.go

--- routes.go
+++ routes.go
@@ -2,6 +2,15 @@ package main
 
 import "net/http"
 
+func send(rw http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+
+		http.ServeFile(rw, r, "resources/send.html")
+	}
+}
+
 func setupRouts() {
 	http.HandleFunc("/send", send)
 	http.ListenAndServe(":8080", nil)
